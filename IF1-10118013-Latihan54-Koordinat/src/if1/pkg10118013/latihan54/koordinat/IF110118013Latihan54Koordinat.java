d8546d9f33de0b59695a818d88941baf8fbef517
Rifian Joe Premilenio <55788355+RifianJoe@users.noreply.github.com>
2019-11-07 21:56:01 +0800

    Add files via upload

---
+++ IF1-10118013-Latihan54-Koordinat/src/if1/pkg10118013/latihan54/koordinat/IF110118013Latihan54Koordinat.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan54.koordinat;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-01
+ * NIM : 10118013
+ */
+public class IF110118013Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Koordinat koordinat = new WarnaKoordinat(10, 4, "Jingga");
+    }
+    
+}
